98a19e1e0fc36ceff42da5b79abfb6876a26088e
sigismallz <ihre anonyme email>
2021-12-03 16:36:43 +0100

    jason erfassung

--- main.py
+++ main.py
@@ -17,11 +17,24 @@ def berechnungen():
 
         return render_template("Berechnungen.html")
 
+@app.route("/berechnungen_addition", methods=["GET", "POST"])
+def berechnungen_addition():
+
+
+        return render_template("Berechnungen_addition.html")
+
+@app.route("/semester_übersicht", methods=["GET", "POST"])
+def semester_übersicht():
+
+
+        return render_template("semester_übersicht.html")
 
 @app.route("/resultat", methods=["GET", "POST"])
 def resultat():
     b = open("Speicher_ECTS.json")
     berechnungen_dict = json.load(b)
+
+
 
     if request.method == "POST":
         if request.form.get("submit_button") == "Senden":
@@ -41,11 +54,20 @@ def resultat():
             berechnungen_dict["ECTS_absolviert"] = absolvierte_ECTS
             berechnungen_dict["semester"] = semester
             berechnungen_dict["faecher_bestanden"] = faecher_bestanden
-            print(berechnungen_dict)
+
+
             with open("Speicher_ECTS.json", "w") as f:
                 json.dump(berechnungen_dict, f, indent=4, separators=(",",":"), sort_keys=True)
-            hurensohn = "SIGI IST EIN HURENSOHN"
-    return render_template("Resultat.html",hurensohn=berechnungen_dict["faecher_bestanden"])
+            if berechnungen_dict["faecher_bestanden"] == "Ja":
+                bestanden = "Du kannst das Studium normal beenden"
+            else:
+                bestanden = "Du kannst das Studium nicht beenden"
+
+            with open("ECTS_ux.json", "w") as f:
+                json.dump(berechnungen_dict, f, indent=4, separators=(",",":"), sort_keys=True)
+                ux_need = 8-int(ECTS_ux)
+                ux_maj = 20 - int(ECTS_ux)
+    return render_template("Resultat.html", bestanden=bestanden,ux=berechnungen_dict["ECTS_ux"], ux_maj=ux_maj, ux_need=ux_need)
 
 
 if __name__ == "__main__":
