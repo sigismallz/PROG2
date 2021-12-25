6ec6ce103134ef3ca7b26b0c3a1119efda3093db
sigismallz <ihre anonyme email>
2021-12-25 16:33:03 +0100

    neue anpassungen

--- main.py
+++ main.py
@@ -13,33 +13,27 @@ def hello_world():
 
 @app.route("/berechnungen", methods=["GET", "POST"])
 def berechnungen():
+    return render_template("Berechnungen.html")
 
-
-        return render_template("Berechnungen.html")
 
 @app.route("/berechnungen_addition", methods=["GET", "POST"])
 def berechnungen_addition():
+    return render_template("Berechnungen_addition.html")
 
-
-        return render_template("Berechnungen_addition.html")
 
 @app.route("/semester_übersicht", methods=["GET", "POST"])
 def semester_übersicht():
+    return render_template("semester_übersicht.html")
 
-
-        return render_template("semester_übersicht.html")
 
 @app.route("/resultat", methods=["GET", "POST"])
 def resultat():
     b = open("Speicher_ECTS.json")
     berechnungen_dict = json.load(b)
 
-
-
     if request.method == "POST":
         if request.form.get("submit_button") == "Senden":
             semester = request.form["semester"]
-
 
             faecher_bestanden = request.form["fächer_bestanden"]
             absolvierte_ECTS = request.form["absolvierte_ECTS"]
@@ -57,19 +51,29 @@ def resultat():
             berechnungen_dict[semester]["semester"] = semester
             berechnungen_dict[semester]["faecher_bestanden"] = faecher_bestanden
 
-
             with open("Speicher_ECTS.json", "w") as f:
-                json.dump(berechnungen_dict, f, indent=4, separators=(",",":"), sort_keys=True)
+                json.dump(berechnungen_dict, f, indent=4, separators=(",", ":"), sort_keys=True)
             if berechnungen_dict[semester]["faecher_bestanden"] == "Ja":
-                bestanden = "Du kannst das Studium normal beenden"
+                bestanden = "Du hast die Möglichkeit dein Studium im geplanten Zeitraum Abzuschliessen"
             else:
-                bestanden = "Du kannst das Studium nicht beenden"
+                bestanden = "Du kannst dein Studium nicht im geplanten Zeitraum Abschliessen"
 
-            with open("ECTS_ux.json", "w") as f:
-                json.dump(berechnungen_dict, f, indent=4, separators=(",",":"), sort_keys=True)
-                ux_need = 8-int(ECTS_ux)
-                ux_maj = 20 - int(ECTS_ux)
-    return render_template("Resultat.html", bestanden=bestanden,ux=berechnungen_dict[semester]["ECTS_ux"], ux_maj=ux_maj, ux_need=ux_need)
+        # with open("ECTS_ux.json", "w") as f:
+        #    json.dump(berechnungen_dict, f, indent=4, separators=(",",":"), sort_keys=True)
+        #   ux_need = 8-int(ECTS_ux)
+        #  ux_maj = 20 - int(ECTS_ux)
+
+
+
+
+    return render_template("Resultat.html", bestanden=bestanden, ux=berechnungen_dict[semester]["ECTS_ux"],
+                           insgesamt=berechnungen_dict[semester]["ECTS_absolviert"],
+                           it=berechnungen_dict[semester]["ECTS_information"],
+                           di=berechnungen_dict[semester]["ECTS_innovation"],
+                           sm=berechnungen_dict[semester]["ECTS_sozial"],
+                           lel=int(berechnungen_dict[semester]["ECTS_absolviert"]) - (int(berechnungen_dict[semester]["ECTS_ux"])+ int(berechnungen_dict[semester]["ECTS_information"])+int(berechnungen_dict[semester]["ECTS_innovation"])+int(berechnungen_dict[semester]["ECTS_sozial"]))
+)
+
 
 
 @app.route("/semesteruebersicht", methods=["GET", "POST"])
@@ -78,9 +82,13 @@ def semesteruebersicht():
     berechnungen_dict = json.load(b)
     semester_output1 = ()
     for semester in berechnungen_dict:
-        semester_output2 = ((berechnungen_dict[semester]["semester"], berechnungen_dict[semester]["ECTS_information"], berechnungen_dict[semester]["ECTS_sozial"], berechnungen_dict[semester]["ECTS_ux"], berechnungen_dict[semester]["ECTS_innovation"], berechnungen_dict[semester]["ECTS_absolviert"]), )
+        semester_output2 = ((berechnungen_dict[semester]["semester"], berechnungen_dict[semester]["ECTS_information"],
+                             berechnungen_dict[semester]["ECTS_sozial"], berechnungen_dict[semester]["ECTS_ux"],
+                             berechnungen_dict[semester]["ECTS_innovation"],
+                             berechnungen_dict[semester]["ECTS_absolviert"]),)
         semester_output1 = semester_output1 + semester_output2
     return render_template("semester_übersicht.html", daten=semester_output1)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
