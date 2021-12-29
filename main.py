a2e5fa69e8d1af34508d721cd98e9b7c074d97ef
sigismallz <ihre anonyme email>
2021-12-29 15:54:19 +0100

    Anpassungen der Berechnungen und Text

--- main.py
+++ main.py
@@ -69,10 +69,10 @@ def resultat():
                            di=berechnungen_dict[semester]["ECTS_innovation"],
                            sm=berechnungen_dict[semester]["ECTS_sozial"],
                            lel=int(berechnungen_dict[semester]["ECTS_absolviert"]) - (
-                                       int(berechnungen_dict[semester]["ECTS_ux"]) + int(
-                                   berechnungen_dict[semester]["ECTS_information"]) + int(
-                                   berechnungen_dict[semester]["ECTS_innovation"]) + int(
-                                   berechnungen_dict[semester]["ECTS_sozial"]))
+                                   int(berechnungen_dict[semester]["ECTS_ux"]) + int(
+                               berechnungen_dict[semester]["ECTS_information"]) + int(
+                               berechnungen_dict[semester]["ECTS_innovation"]) + int(
+                               berechnungen_dict[semester]["ECTS_sozial"]))
                            )
 
 
@@ -82,15 +82,45 @@ def semesteruebersicht():
     berechnungen_dict = json.load(b)
     semester_output1 = ()
 
-    it = int(20) - (int(berechnungen_dict["1"]["ECTS_information"]) + int(berechnungen_dict["2"]["ECTS_information"]) + int(berechnungen_dict["3"]["ECTS_information"]) + int(berechnungen_dict["4"]["ECTS_information"]) + int(berechnungen_dict["5"]["ECTS_information"]) + int(berechnungen_dict["6"]["ECTS_information"]))
+    it = int(20) - (
+                int(berechnungen_dict["1"]["ECTS_information"]) + int(berechnungen_dict["2"]["ECTS_information"]) + int(
+            berechnungen_dict["3"]["ECTS_information"]) + int(berechnungen_dict["4"]["ECTS_information"]) + int(
+            berechnungen_dict["5"]["ECTS_information"]) + int(berechnungen_dict["6"]["ECTS_information"]))
     if it < 0:
         it = 0
-    di = int(20) - (int(berechnungen_dict["1"]["ECTS_innovation"]) + int(berechnungen_dict["2"]["ECTS_innovation"]) + int(berechnungen_dict["3"]["ECTS_innovation"]) + int(berechnungen_dict["4"]["ECTS_innovation"]) + int(berechnungen_dict["5"]["ECTS_innovation"]) + int(berechnungen_dict["6"]["ECTS_innovation"]))
+    di = int(20) - (
+                int(berechnungen_dict["1"]["ECTS_innovation"]) + int(berechnungen_dict["2"]["ECTS_innovation"]) + int(
+            berechnungen_dict["3"]["ECTS_innovation"]) + int(berechnungen_dict["4"]["ECTS_innovation"]) + int(
+            berechnungen_dict["5"]["ECTS_innovation"]) + int(berechnungen_dict["6"]["ECTS_innovation"]))
     if di < 0:
         di = 0
     ux = int(20) - (int(berechnungen_dict["1"]["ECTS_ux"]) + int(berechnungen_dict["2"]["ECTS_ux"]) + int(berechnungen_dict["3"]["ECTS_ux"]) + int(berechnungen_dict["4"]["ECTS_ux"]) + int(berechnungen_dict["5"]["ECTS_ux"]) + int(berechnungen_dict["6"]["ECTS_ux"]))
     if ux < 0:
         ux = 0
+
+    it_1 = int(8) - (
+                int(berechnungen_dict["1"]["ECTS_information"]) + int(berechnungen_dict["2"]["ECTS_information"]) + int(
+            berechnungen_dict["3"]["ECTS_information"]) + int(berechnungen_dict["4"]["ECTS_information"]) + int(
+            berechnungen_dict["5"]["ECTS_information"]) + int(berechnungen_dict["6"]["ECTS_information"]))
+    if it_1 < 0:
+        it_1 = 0
+    di_1 = int(20) - (
+            int(berechnungen_dict["1"]["ECTS_innovation"]) + int(berechnungen_dict["2"]["ECTS_innovation"]) + int(
+        berechnungen_dict["3"]["ECTS_innovation"]) + int(berechnungen_dict["4"]["ECTS_innovation"]) + int(
+        berechnungen_dict["5"]["ECTS_innovation"]) + int(berechnungen_dict["6"]["ECTS_innovation"]))
+    if di_1 < 0:
+        di_1 = 0
+    ux_1 = int(20) - (int(berechnungen_dict["1"]["ECTS_ux"]) + int(berechnungen_dict["2"]["ECTS_ux"]) + int(
+        berechnungen_dict["3"]["ECTS_ux"]) + int(berechnungen_dict["4"]["ECTS_ux"]) + int(
+        berechnungen_dict["5"]["ECTS_ux"]) + int(berechnungen_dict["6"]["ECTS_ux"]))
+    if ux_1 < 0:
+        ux_1 = 0
+    sz = int(4) - (int(berechnungen_dict["1"]["ECTS_sozial"]) + int(berechnungen_dict["2"]["ECTS_sozial"]) + int(
+        berechnungen_dict["3"]["ECTS_sozial"]) + int(berechnungen_dict["4"]["ECTS_sozial"]) + int(
+        berechnungen_dict["5"]["ECTS_sozial"]) + int(berechnungen_dict["6"]["ECTS_sozial"]))
+    if sz < 0:
+        sz = 0
+
     for semester in berechnungen_dict:
         semester_output2 = ((berechnungen_dict[semester]["semester"], berechnungen_dict[semester]["ECTS_information"],
                              berechnungen_dict[semester]["ECTS_sozial"], berechnungen_dict[semester]["ECTS_ux"],
@@ -98,12 +128,19 @@ def semesteruebersicht():
                              berechnungen_dict[semester]["ECTS_absolviert"]),)
         semester_output1 = semester_output1 + semester_output2
     return render_template("semester_übersicht.html", daten=semester_output1,
-                           insgesamt=int(180) - (int(berechnungen_dict["1"]["ECTS_absolviert"]) + int(berechnungen_dict["2"]["ECTS_absolviert"]) + int(berechnungen_dict["3"]["ECTS_absolviert"]) + int(berechnungen_dict["4"]["ECTS_absolviert"]) + int(berechnungen_dict["5"]["ECTS_absolviert"]) + int(berechnungen_dict["6"]["ECTS_absolviert"])),
+                           insgesamt=int(180) - (int(berechnungen_dict["1"]["ECTS_absolviert"]) + int(
+                               berechnungen_dict["2"]["ECTS_absolviert"]) + int(
+                               berechnungen_dict["3"]["ECTS_absolviert"]) + int(
+                               berechnungen_dict["4"]["ECTS_absolviert"]) + int(
+                               berechnungen_dict["5"]["ECTS_absolviert"]) + int(
+                               berechnungen_dict["6"]["ECTS_absolviert"])),
                            it=it,
                            di=di,
-                           ux=ux
-
-                           )
+                           ux=ux,
+                           it_1=it_1,
+                           di_1=di_1,
+                           ux_1=ux_1,
+                           sz=sz)
 
 
 if __name__ == "__main__":
