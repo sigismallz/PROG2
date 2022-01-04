cfe1fe05545b50d37d66ec63e034bffd2b3ad091
sigismallz <ihre anonyme email>
2022-01-04 17:32:35 +0100

    code angepast und Readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-** HALLO Das ist mein README.md **
-viel spass mit meinem Projekt
+<h1>HALLO das ist mein README.md </h1>
+
+<h3>In diesem Programm hast du die Möglichkeit die errungenen ECTS einzutragen. Zuerst trägst du die ECTS ein, die du insgesamt in diesem Semester absolviert hast. Danach die ECTS der  Wahlpflichtmodule.
+		Dies kannst du  für jedes Semester einzel machen. 
+<p>
+		Hast du das aktuelle Semester eingetragen, bekommst du eine Zussammenfassung bezüglich des Semesters. Auf der letzten Seite giebt es eine Tabelle die dein Studium Zusammenfast.
+		Auch wirst du darüber Informiert wie viele ECTS du in den Wahlpflichtmodulen noch absolvieren musst, um einen Major zu erhalzten.
+		Des weiteren  wirst du darüber informiert wie viele ECTS du in den Vertiefungen noch mindestens absolviern musst um das Studium zu beenden.
+<p>
+		In den Vertiefungen Information Technology, User Experience und Digital Innovation benötigst du mindestens 8 ECTS um das Studium zu beenden..
+		Ab 20 ECTS erhälts du einen Major in dieser vertiefung.
+<p>
+		In der Vertiefung Sozial- und Methodenkompetenz kannst du keinen Major machen. Du benöttigts auch lediglich 4 ECTS um das Studium zu Beenden.
--- main.py
+++ main.py
@@ -16,52 +16,39 @@ def berechnungen():
     return render_template("Berechnungen.html")
 
 
-@app.route("/berechnungen_addition", methods=["GET", "POST"])
-def berechnungen_addition():
-    return render_template("Berechnungen_addition.html")
-
-
-@app.route("/semester_übersicht", methods=["GET", "POST"])
-def semester_übersicht():
-    return render_template("semester_übersicht.html")
-
-
 @app.route("/resultat", methods=["GET", "POST"])
 def resultat():
     b = open("Speicher_ECTS.json")
     berechnungen_dict = json.load(b)
 
-    if request.method == "POST":
-        if request.form.get("submit_button") == "Senden":
-            semester = request.form["semester"]
+    if request.method == "POST" and request.form.get("submit_button") == "Senden":
+        semester = request.form["semester"]
 
-            faecher_bestanden = request.form["fächer_bestanden"]
-            absolvierte_ECTS = request.form["absolvierte_ECTS"]
-            ECTS_sozial = request.form["ECTS_sozial"]
-            ECTS_information = request.form["ECTS_information"]
-            ECTS_ux = request.form["ECTS_ux"]
-            ECTS_innovation = request.form["ECTS_innovation"]
+        faecher_bestanden = request.form["fächer_bestanden"]
+        absolvierte_ECTS = request.form["absolvierte_ECTS"]
+        ECTS_sozial = request.form["ECTS_sozial"]
+        ECTS_information = request.form["ECTS_information"]
+        ECTS_ux = request.form["ECTS_ux"]
+        ECTS_innovation = request.form["ECTS_innovation"]
 
-            berechnungen_dict[semester]["ECTS_information"] = ECTS_information
-            berechnungen_dict[semester]["ECTS_innovation"] = ECTS_innovation
-            berechnungen_dict[semester]["ECTS_information"] = ECTS_information
-            berechnungen_dict[semester]["ECTS_sozial"] = ECTS_sozial
-            berechnungen_dict[semester]["ECTS_ux"] = ECTS_ux
-            berechnungen_dict[semester]["ECTS_absolviert"] = absolvierte_ECTS
-            berechnungen_dict[semester]["semester"] = semester
-            berechnungen_dict[semester]["faecher_bestanden"] = faecher_bestanden
+        berechnungen_dict[semester]["ECTS_information"] = ECTS_information
+        berechnungen_dict[semester]["ECTS_innovation"] = ECTS_innovation
+        berechnungen_dict[semester]["ECTS_information"] = ECTS_information
+        berechnungen_dict[semester]["ECTS_sozial"] = ECTS_sozial
+        berechnungen_dict[semester]["ECTS_ux"] = ECTS_ux
+        berechnungen_dict[semester]["ECTS_absolviert"] = absolvierte_ECTS
+        berechnungen_dict[semester]["semester"] = semester
+        berechnungen_dict[semester]["faecher_bestanden"] = faecher_bestanden
 
-            with open("Speicher_ECTS.json", "w") as f:
-                json.dump(berechnungen_dict, f, indent=4, separators=(",", ":"), sort_keys=True)
-            if berechnungen_dict[semester]["faecher_bestanden"] == "Ja":
+        with open("Speicher_ECTS.json", "w") as f:
+            json.dump(berechnungen_dict, f, indent=4, separators=(",", ":"), sort_keys=True)
+        if berechnungen_dict[semester]["faecher_bestanden"] != "Nein":
+            bestanden = "Du hast die Möglichkeit dein Studium im geplanten Zeitraum Abzuschliessen"
+        elif int(berechnungen_dict[semester]["semester"]) < int(5):
                 bestanden = "Du hast die Möglichkeit dein Studium im geplanten Zeitraum Abzuschliessen"
-            else:
-                bestanden = "Du kannst dein Studium nicht im geplanten Zeitraum Abschliessen"
+        else:
+            bestanden = "Du kannst dein Studium nicht im geplanten Zeitraum Abschliessen"
 
-        # with open("ECTS_ux.json", "w") as f:
-        #    json.dump(berechnungen_dict, f, indent=4, separators=(",",":"), sort_keys=True)
-        #   ux_need = 8-int(ECTS_ux)
-        #  ux_maj = 20 - int(ECTS_ux)
 
     return render_template("Resultat.html", bestanden=bestanden, ux=berechnungen_dict[semester]["ECTS_ux"],
                            insgesamt=berechnungen_dict[semester]["ECTS_absolviert"],
