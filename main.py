5f1519bc2a9f4ddd653ffde1368c41d42597d7bc
sigismallz <ihre anonyme email>
2022-01-05 20:29:09 +0100

    Ploty hinzugefügt

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask
 import json
 from flask import render_template
 from flask import request
+import plotly.graph_objects as go
+
+
 
 app = Flask("Hello World")
 
@@ -11,7 +14,7 @@ def hello_world():
     b = open("Speicher_ECTS.json")
     berechnungen_dict = json.load(b)
     if request.method == "POST":
-        #ermöglicht das Zurücksetzen des json Dictionary
+        # ermöglicht das Zurücksetzen des json Dictionary
         if request.form.get("button_name") == "Resultate Zurücksetzen":
             berechnungen_dict["1"]["ECTS_absolviert"] = "0"
             berechnungen_dict["1"]["ECTS_information"] = "0"
@@ -89,7 +92,7 @@ def resultat():
         berechnungen_dict[semester]["semester"] = semester
         berechnungen_dict[semester]["faecher_bestanden"] = faecher_bestanden
 
-#Ab dem 5 Semester ist es nicht mehr möglich das Studium im normalen Zeitraum zu Beenden
+        # Ab dem 5 Semester ist es nicht mehr möglich das Studium im normalen Zeitraum zu Beenden
         with open("Speicher_ECTS.json", "w") as f:
             json.dump(berechnungen_dict, f, indent=4, separators=(",", ":"), sort_keys=True)
         if berechnungen_dict[semester]["faecher_bestanden"] != "Nein":
@@ -117,7 +120,7 @@ def semesteruebersicht():
     b = open("Speicher_ECTS.json")
     berechnungen_dict = json.load(b)
     semester_output1 = ()
-# Berechnungen für die Übersicht
+    # Berechnungen für die Übersicht
     it = int(20) - (
             int(berechnungen_dict["1"]["ECTS_information"]) + int(berechnungen_dict["2"]["ECTS_information"]) + int(
         berechnungen_dict["3"]["ECTS_information"]) + int(berechnungen_dict["4"]["ECTS_information"]) + int(
@@ -158,7 +161,8 @@ def semesteruebersicht():
         berechnungen_dict["5"]["ECTS_sozial"]) + int(berechnungen_dict["6"]["ECTS_sozial"]))
     if sz < 0:
         sz = 0
-# Methode zur generierung der Daten für die Tabelle
+
+    # Methode zur generierung der Daten für die Tabelle
     for semester in berechnungen_dict:
         semester_output2 = ((berechnungen_dict[semester]["semester"], berechnungen_dict[semester]["ECTS_information"],
                              berechnungen_dict[semester]["ECTS_sozial"], berechnungen_dict[semester]["ECTS_ux"],
@@ -178,7 +182,32 @@ def semesteruebersicht():
                            it_1=it_1,
                            di_1=di_1,
                            ux_1=ux_1,
-                           sz=sz)
+                           sz=sz,
+                           )
+
+
+
+
+
+@app.route("/plotly", methods=["GET", "POST"])
+def plotly():
+    # Datenvisualiesierung mit Ploty
+    b = open("Speicher_ECTS.json")
+    berechnungen_dict = json.load(b)
+
+    graph = go.Figure(data=go.Bar(
+        x=[1, 2, 3, 4, 5, 6],
+        y=[int(berechnungen_dict["1"]["ECTS_absolviert"]), int(berechnungen_dict["2"]["ECTS_absolviert"]),
+           int(berechnungen_dict["3"]["ECTS_absolviert"]), int(berechnungen_dict["4"]["ECTS_absolviert"]),
+           int(berechnungen_dict["5"]["ECTS_absolviert"]), int(berechnungen_dict["6"]["ECTS_absolviert"])]
+    ),
+        layout=go.Layout(
+            title=go.layout.Title(text="Visualisierung der absolvierten ECTS")
+        )
+    )
+    graph.write_html("templates/Plotly.html", auto_open=False)
+
+    return render_template("Plotly.html")
 
 
 if __name__ == "__main__":
